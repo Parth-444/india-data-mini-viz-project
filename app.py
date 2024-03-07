04aaf143f74e176c851ec00ed620c0551fbea322
P4rth <95160360+Parth-444@users.noreply.github.com>
2024-03-07 19:46:24 +0530

    Update app.py
    Remove Extra Lines that shows up when options other than Overall India is Selected

--- app.py
+++ app.py
@@ -26,8 +26,6 @@ if plot:
                                 mapbox_style='carto-positron', width=1200, height=700)
         st.plotly_chart(fig, use_container_width=True)
     else:
-        st.text('Size represents Primary parameter')
-        st.text('Color represents secondary parameter')
         state_df = df[df['State'] == selected_state]
         fig = px.scatter_mapbox(state_df, lat='Latitude', lon='Longitude', size=primary,
                                 color=secondary, zoom=6, size_max=35, hover_name='District',
